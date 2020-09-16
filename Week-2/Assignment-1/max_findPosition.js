97ba9191405fb8ea521fc018cae339f9b6c539ea
victorwung <fixedstar1350@gmail.com>
2020-09-16 15:05:17 +0800

    first commit

---
+++ Week-2/Assignment-1/max_findPosition.js
@@ -0,0 +1,31 @@
+function max(numbers) {
+    let cur_max = 0;
+    for (var i = 0; i < numbers.length; i++) {
+        if (numbers[i] > cur_max) {
+            cur_max = numbers[i]; // if larger than cur_max, update cur_max
+        }
+        else {
+            // pass
+        }
+    }
+    return cur_max;
+}
+function findPosition(numbers, target) {
+    let target_index = -1; // default as -1
+    for (var i = 0; i < numbers.length; i++) {
+        if (numbers[i] === target) {
+            target_index = i; // if match target return the array index
+            break;
+        }
+        else {
+            // pass
+        }
+    }
+    return target_index;
+}
+console.log( max([1, 2, 4, 5]) ); // should print 5
+console.log( max([5, 2, 7, 1, 6]) ); // should print 7
+console.log( findPosition([5, 2, 7, 1, 6], 5) ); // should print 0
+console.log( findPosition([5, 2, 7, 1, 6], 7) ); // should print 2
+console.log( findPosition([5, 2, 7, 7, 7, 1, 6], 7) ); // should print 2 (the first position)
+console.log( findPosition([5, 2, 7, 1, 6], 8) ); // should print -1
